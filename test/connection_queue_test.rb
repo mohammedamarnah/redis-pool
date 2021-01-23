8a15fff264fff00533f08fd3f279bdc1db5917ac
Mohammed Amarnah <m.amarnah@gmail.com>
2021-01-23 15:09:36 +0200

    add more tests for connections with timeouts

--- test/connection_queue_test.rb
+++ test/connection_queue_test.rb
@@ -31,4 +31,23 @@ class ConnectionQueueTest < Minitest::Test
     assert_equal q.total_available, max_size
     assert_equal q.available_to_create, max_size - 1
   end
+
+  def test_checking_out_with_timeout
+    max_size = 1
+    q = ConnectionQueue.new(max_size, &redis_block)
+    q.poll
+    assert_raises TimeoutError do
+      q.poll 0
+    end
+  end
+
+  def test_checking_out_then_in_with_timeout
+    max_size = 1
+    q = ConnectionQueue.new(max_size, &redis_block)
+    conn = q.poll
+    q.add conn
+    q.poll 0
+    assert_equal q.total_available, 0
+    assert_equal q.available_to_create, 0
+  end
 end
